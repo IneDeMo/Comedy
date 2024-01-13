d0e05263e87ea2268a3ff1c7ef3af2a7fc3ef6ec
Ine De Moerloose <r0886600@student.vives.be>
2024-01-14 00:24:46 +0100

    first change check

---
+++ script.js
@@ -0,0 +1,5 @@
+﻿// script.js
+document.addEventListener('DOMContentLoaded', function () {
+    // Voeg hier je JavaScript-code toe
+    alert('Welkom bij mijn website! Dit is een JavaScript-alert.');
+});
